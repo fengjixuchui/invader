605412e8a8f95f63f9ede838e61eeb186eee7a52
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-16 21:58:32 +0200

    Add files via upload

---
+++ setup/cert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# generate a self-signed CERT
+#openssl genrsa -des3 -out ./data/invader.orig.key 2048
+#openssl rsa -in ./data/invader.orig.key -out ./data/invader.key
+#openssl req -new -key ./data/invader.key -out ./data/invader.csr
+#openssl x509 -req -days 365 -in ./data/invader.csr -signkey ./data/invader.key -out ./data/invader.crt
+
+#openssl req -new -x509 -keyout ../data/invader-priv.key -out ../data/invader-chain.pem -days 365 -nodes
+openssl req -new -x509 -keyout ../data/invader-priv.key -out ../data/invader-chain.pem -days 365 -nodes -subj "/C=US" >/dev/null 2>&1
+
+echo -e "\n [*] Certificate written to ../data/invader-chain.pem"
+echo -e "\r [*] Private key written to ../data/invader-priv.key\n"
---
+++ setup/reset.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo " [!]This script must be run as root" 1>&2
+   exit 1
+fi
+
+IFS='/' read -a array <<< pwd
+
+if [[ "$(pwd)" != *setup ]]
+then
+	cd ./setup
+fi
+
+# reset the database
+if [ -e ../data/invader.db ]
+then
+	rm ../data/invader.db
+fi
+
+python ./setup_database.py
+cd ..
+
+# remove the debug file if it exists
+if [ -e invader.debug ]
+then
+	rm invader.debug
+fi
+
+# remove the download folders
+if [ -d ./downloads/ ]
+then
+	rm -rf ./downloads/
+fi
+
+# start up invader if not in docker otherwise return
+if [ -f /.dockerenv ]; then
+    echo " [*] invader reset complete returning back to Docker"
+else
+    ./invader
+fi
