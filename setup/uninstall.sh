064ff698ca4876f31530b5ea6c782dc2457db2af
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-18 18:49:09 +0200

    Update uninstall.sh

--- setup/uninstall.sh
+++ setup/uninstall.sh
@@ -1,7 +1,10 @@
+#! /bin/bash
 sleep 1
+
 {
 rm /bin/invader
 rm /usr/local/bin/invader
 rm -r ~/invader
-}&> /dev/null
-echo -e "\033[1;32mUninstallation completed\033[0m"
+} &> /dev/null
+
+echo -e "\033[1;32mUninstallation completed!\033[0m"
