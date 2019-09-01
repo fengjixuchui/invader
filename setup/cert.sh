557e5d670198d21e7e419255407d67fee8629be2
Ivan Nikolsky <54115104+entydev@users.noreply.github.com>
2019-09-01 19:43:05 +0300

    Update cert.sh

--- setup/cert.sh
+++ setup/cert.sh
@@ -23,12 +23,12 @@
 # SOFTWARE.
 
 # generate a self-signed CERT
-#openssl genrsa -des3 -out ./data/Invader.orig.key 2048
-#openssl rsa -in ./data/Invader.orig.key -out ./data/Invader.key
-#openssl req -new -key ./data/Invader.key -out ./data/Invader.csr
-#openssl x509 -req -days 365 -in ./data/Invader.csr -signkey ./data/Invader.key -out ./data/Invader.crt
+# openssl genrsa -des3 -out ./data/invader.orig.key 2048
+# openssl rsa -in ./data/invader.orig.key -out ./data/invader.key
+# openssl req -new -key ./data/invader.key -out ./data/invader.csr
+# openssl x509 -req -days 365 -in ./data/invader.csr -signkey ./data/invader.key -out ./data/invader.crt
 
-#openssl req -new -x509 -keyout ../data/invader-priv.key -out ../data/invader-chain.pem -days 365 -nodes
+# openssl req -new -x509 -keyout ../data/invader-priv.key -out ../data/invader-chain.pem -days 365 -nodes
 openssl req -new -x509 -keyout ../data/invader-priv.key -out ../data/invader-chain.pem -days 365 -nodes -subj "/C=US" >/dev/null 2>&1
 
 echo -e "\033[1;32m[*] Certificate written to ../data/invader-chain.pem"
