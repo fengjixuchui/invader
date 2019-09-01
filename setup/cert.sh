b4a864446c21158500967b95eaa5f147c94043b9
Ivan Nikolsky <54115104+entydev@users.noreply.github.com>
2019-09-01 16:30:17 +0300

    Add files via upload

--- setup/cert.sh
+++ setup/cert.sh
@@ -28,8 +28,8 @@
 #openssl req -new -key ./data/Invader.key -out ./data/Invader.csr
 #openssl x509 -req -days 365 -in ./data/Invader.csr -signkey ./data/Invader.key -out ./data/Invader.crt
 
-#openssl req -new -x509 -keyout ../data/Invader-priv.key -out ../data/Invader-chain.pem -days 365 -nodes
-openssl req -new -x509 -keyout ../data/Invader-priv.key -out ../data/Invader-chain.pem -days 365 -nodes -subj "/C=US" >/dev/null 2>&1
+#openssl req -new -x509 -keyout ../data/invader-priv.key -out ../data/invader-chain.pem -days 365 -nodes
+openssl req -new -x509 -keyout ../data/invader-priv.key -out ../data/invader-chain.pem -days 365 -nodes -subj "/C=US" >/dev/null 2>&1
 
-echo -e "\033[1;32m[*] Certificate written to ../data/Invader-chain.pem"
-echo -e "[*] Private key written to ../data/Invader-priv.key\033[0m"
+echo -e "\033[1;32m[*] Certificate written to ../data/invader-chain.pem"
+echo -e "[*] Private key written to ../data/invader-priv.key\033[0m"
